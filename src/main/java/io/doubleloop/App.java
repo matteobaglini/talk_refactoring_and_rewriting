54d8e48b71bb7335ce61c356a778b32df2c348a5
Matteo Baglini <matteo.baglini@gmail.com>
2024-01-16 09:17:18 +0100

    init

---
+++ src/main/java/io/doubleloop/App.java
@@ -0,0 +1,7 @@
+package io.doubleloop;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/io/doubleloop/AppTest.java
@@ -0,0 +1,11 @@
+package io.doubleloop;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+  @Test
+  void itWorks() {
+    assertThat(true).isTrue();
+  }
+}
