7bde93b451e00d44ff2bf2a7ef5376bdbba34c62
Matteo Baglini <matteo.baglini@gmail.com>
2024-01-17 00:22:49 +0100

    test: pulls setup and dsl functions up in contact test suite

--- src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOnContractTest.java
+++ src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOnContractTest.java
@@ -1,3 +1,42 @@
 package io.doubleloop;
 
-public class LoadEmployeesCelebratingBirthdayOnContractTest {}
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+public class LoadEmployeesCelebratingBirthdayOnContractTest {
+  private Path testDataPath;
+
+  @BeforeEach
+  void setUp() {
+    testDataPath = Paths.get("test-data-bs");
+    final var testDataDir = testDataPath.toFile();
+    testDataDir.deleteOnExit();
+    testDataDir.mkdirs();
+  }
+
+  protected String employeeFile(String... lines) throws IOException {
+    final var filePath = Paths.get(testDataPath.toString(), "employee_data.txt");
+    filePath.toFile().deleteOnExit();
+    Files.write(filePath, Arrays.asList(lines));
+    return filePath.toString();
+  }
+
+  protected String line(final String text) {
+    return text;
+  }
+
+  protected String header() {
+    return "last_name, first_name, date_of_birth, email";
+  }
+
+  protected LocalDate date(String value) {
+    return LocalDate.parse(value, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+  }
+}
--- src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_OldTest.java
+++ src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_OldTest.java
@@ -2,27 +2,10 @@ package io.doubleloop;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LoadEmployeesCelebratingBirthdayOn_OldTest
     extends LoadEmployeesCelebratingBirthdayOnContractTest {
-  private Path testDataPath;
-
-  @BeforeEach
-  void setUp() {
-    testDataPath = Paths.get("test-data-bs");
-    final var testDataDir = testDataPath.toFile();
-    testDataDir.deleteOnExit();
-    testDataDir.mkdirs();
-  }
 
   @Test
   void noEmployeesCelebrateBirthday() throws Exception {
@@ -35,22 +18,4 @@ class LoadEmployeesCelebratingBirthdayOn_OldTest
     assertThat(employees.size()).isEqualTo(0);
   }
 
-  private String employeeFile(String... lines) throws IOException {
-    final var filePath = Paths.get(testDataPath.toString(), "employee_data.txt");
-    filePath.toFile().deleteOnExit();
-    Files.write(filePath, Arrays.asList(lines));
-    return filePath.toString();
-  }
-
-  private String line(final String text) {
-    return text;
-  }
-
-  private String header() {
-    return "last_name, first_name, date_of_birth, email";
-  }
-
-  private LocalDate date(String value) {
-    return LocalDate.parse(value, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-  }
 }
