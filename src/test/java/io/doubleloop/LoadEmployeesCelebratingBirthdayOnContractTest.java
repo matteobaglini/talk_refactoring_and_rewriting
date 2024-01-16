d65874df877a0000a95cf1f9d4eaed5b9d1ebba2
Matteo Baglini <matteo.baglini@gmail.com>
2024-01-17 00:33:16 +0100

    test: pull more characterization test up into contract test suite

--- src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOnContractTest.java
+++ src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOnContractTest.java
@@ -55,4 +55,33 @@ public abstract class LoadEmployeesCelebratingBirthdayOnContractTest {
   }
 
   protected abstract LoadEmployeesCelebratingBirthdayOnContract createLoadEmployeesCelebratingBirthdayOn(String filePath, LocalDate today);
+
+  @Test
+  void oneEmployeeCelebrateBirthday() throws Exception {
+    final var filePath = employeeFile(
+        header(),
+        line("Doe, John, 1982/10/08, john.doe@foobar.com"),
+        line("Ann, Mary, 1975/03/11, mary.ann@foobar.com")
+    );
+    final var employees = createLoadEmployeesCelebratingBirthdayOn(filePath, date("2024/10/08")).execute();
+
+    assertThat(employees.size()).isEqualTo(1);
+    assertThat(employees.get(0).getFirstName()).isEqualTo("John");
+    assertThat(employees.get(0).getEmail()).isEqualTo("john.doe@foobar.com");
+  }
+
+  @Test
+  void manyEmployeesCelebrateBirthday() throws Exception {
+    final var filePath = employeeFile(
+        header(),
+        line("Doe, John, 1982/10/08, john.doe@foobar.com"),
+        line("Ann, Mary, 1975/03/11, mary.ann@foobar.com"),
+        line("Rossi, Mario, 2000/10/08, mario.rossi@foobar.com")
+    );
+    final var employees =createLoadEmployeesCelebratingBirthdayOn(filePath, date("2024/10/08")).execute();
+
+    assertThat(employees.size()).isEqualTo(2);
+    assertThat(employees.get(0).getFirstName()).isEqualTo("John");
+    assertThat(employees.get(1).getFirstName()).isEqualTo("Mario");
+  }
 }
