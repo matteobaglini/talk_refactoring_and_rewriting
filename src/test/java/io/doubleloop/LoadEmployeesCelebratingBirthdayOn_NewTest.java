c2a629e22945463df570f2f79cad775d1c8f30e4
Matteo Baglini <matteo.baglini@gmail.com>
2024-01-17 00:28:08 +0100

    test: define test suite for new implementation

---
+++ src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_NewTest.java
@@ -0,0 +1,11 @@
+package io.doubleloop;
+
+import java.time.LocalDate;
+
+public class LoadEmployeesCelebratingBirthdayOn_NewTest
+    extends LoadEmployeesCelebratingBirthdayOnContractTest {
+  @Override
+  protected LoadEmployeesCelebratingBirthdayOnContract createLoadEmployeesCelebratingBirthdayOn(String filePath, LocalDate today) {
+    return new LoadEmployeesCelebratingBirthdayOn_New(filePath, today);
+  }
+}
