6c55747f40cfe8f6053385109a6b00d288d319d5
Matteo Baglini <matteo.baglini@gmail.com>
2024-04-17 12:46:44 +0200

    test - contract testing: full new implementation

--- src/main/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_New.java
+++ src/main/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_New.java
@@ -1,17 +1,29 @@
 package io.doubleloop;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
 public class LoadEmployeesCelebratingBirthdayOn_New implements LoadEmployeesCelebratingBirthdayOnContract {
 
+  private final String fileName;
+  private final LocalDate today;
+
   public LoadEmployeesCelebratingBirthdayOn_New(String fileName, LocalDate today) {
+    this.fileName = fileName;
+    this.today = today;
   }
 
   @Override
   public List<Employee> execute() throws IOException {
-    return Collections.emptyList();
+    final int HEADER = 1;
+    return Files.readAllLines(Path.of(fileName)).stream()
+        .skip(HEADER)
+        .map(Employee::parseLine)
+        .filter(e -> e.isBirthday(today))
+        .toList();
   }
 }
