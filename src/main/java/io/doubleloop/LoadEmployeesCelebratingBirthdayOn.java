bb07e6e470841858d7fb98090e036158e1b83415
Matteo Baglini <matteo.baglini@gmail.com>
2024-04-17 14:30:51 +0200

    test - shadow tests: prepare to use new impl

--- src/main/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn.java
+++ src/main/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn.java
@@ -7,12 +7,16 @@ import java.util.List;
 public class LoadEmployeesCelebratingBirthdayOn {
 
   private final LoadEmployeesCelebratingBirthdayOn_Old old;
+  private final LoadEmployeesCelebratingBirthdayOn_New candidate;
 
   public LoadEmployeesCelebratingBirthdayOn(String fileName, LocalDate today) {
     old = new LoadEmployeesCelebratingBirthdayOn_Old(fileName, today);
+    candidate = new LoadEmployeesCelebratingBirthdayOn_New(fileName, today);
   }
 
   public List<Employee> execute() throws IOException {
+    // replace or not? that is the question :-)
     return old.execute();
+    // return candidate.execute();
   }
 }
