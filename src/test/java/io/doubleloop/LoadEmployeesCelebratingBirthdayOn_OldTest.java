7a21dc9105971b7c28e2141c47c6ffbcb736c023
Matteo Baglini <matteo.baglini@gmail.com>
2024-04-17 12:47:55 +0200

    test - contract testing: characterization test bad path for old implementation

--- src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_OldTest.java
+++ src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_OldTest.java
@@ -1,11 +1,23 @@
 package io.doubleloop;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 // REFACTORING: introduce contract tests
 
 public class LoadEmployeesCelebratingBirthdayOn_OldTest extends LoadEmployeesCelebratingBirthdayOnContractTest {
+  @Test
+  void missingFile() throws Exception {
+    final var filePath = "unknown.txt";
 
+    assertThatThrownBy(() -> {
+      createLoadEmployeesCelebratingBirthdayOn(filePath, "2024/10/08").execute();
+    }).isInstanceOf(FileNotFoundException.class)
+        .hasMessageContaining(filePath);
+  }
   @Override
   protected LoadEmployeesCelebratingBirthdayOnContract createLoadEmployeesCelebratingBirthdayOn(String filePath, String today) {
     return new LoadEmployeesCelebratingBirthdayOn_Old(filePath, date(today));
