f1bb5a926fd5c78f715904feb6df9159a9145e0c
Matteo Baglini <matteo.baglini@gmail.com>
2024-04-17 14:08:37 +0200

    test - approval tests: add combination tests

--- src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_OldTest.java
+++ src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_OldTest.java
@@ -1,6 +1,9 @@
 package io.doubleloop;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.approvaltests.combinations.CombinationApprovals;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 // REFACTORING: gain more coverage with ApprovalTests
 
@@ -10,4 +13,24 @@ public class LoadEmployeesCelebratingBirthdayOn_OldTest extends LoadEmployeesCel
     return new LoadEmployeesCelebratingBirthdayOn_Old(filePath, date(today));
   }
 
+  @Test
+  void combinationTests() throws Exception {
+    final var filePath =
+        employeeFile(
+            header(),
+            line("Doe, John, 1982/10/08, john.doe@foobar.com"),
+            line("Ann, Mary, 1975/03/11, mary.ann@foobar.com"),
+            line("Rossi, Mario, 2000/10/08, mario.rossi@foobar.com"));
+
+    CombinationApprovals.verifyAllCombinations(
+        (file, today) -> {
+          try {
+            return createLoadEmployeesCelebratingBirthdayOn(file, today).execute();
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        },
+        new String[] {filePath},
+        new String[] {"2024/10/08", "2024/10/09", "2024/03/11"});
+  }
 }
