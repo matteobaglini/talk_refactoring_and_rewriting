6637eea55c0421ee76b64b1b0bf1f3c3382cae25
Matteo Baglini <matteo.baglini@gmail.com>
2024-04-17 14:37:59 +0200

    final clean up: remove old impl tests

--- src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_NewTest.java
+++ src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_NewTest.java
@@ -1,8 +1,119 @@
 package io.doubleloop;
 
-public class LoadEmployeesCelebratingBirthdayOn_NewTest extends LoadEmployeesCelebratingBirthdayOnContractTest {
-  @Override
-  protected LoadEmployeesCelebratingBirthdayOnContract createLoadEmployeesCelebratingBirthdayOn(String filePath, String today) {
-    return new LoadEmployeesCelebratingBirthdayOn_New(filePath, date(today));
+import org.approvaltests.combinations.CombinationApprovals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LoadEmployeesCelebratingBirthdayOn_NewTest {
+  private Path testDataPath;
+
+  @BeforeEach
+  void setUp() {
+    testDataPath = Paths.get("test-data-bs");
+    final var testDataDir = testDataPath.toFile();
+    testDataDir.deleteOnExit();
+    testDataDir.mkdirs();
+  }
+
+  protected String employeeFile(String... lines) throws IOException {
+    final var filePath = Paths.get(testDataPath.toString(), "employee_data.txt");
+    filePath.toFile().deleteOnExit();
+    Files.write(filePath, Arrays.asList(lines));
+    return filePath.toString();
+  }
+
+  @Test
+  void noEmployeeCelebrateBirthday() throws Exception {
+    final var filePath = employeeFile(
+        header(),
+        line("Ann, Mary, 1975/03/11, mary.ann@foobar.com")
+    );
+    final var employees = new LoadEmployeesCelebratingBirthdayOn_New(filePath, date("2024/10/08")).execute();
+
+    assertThat(employees.size()).isEqualTo(0);
+  }
+
+  @Test
+  void missingFile() throws Exception {
+    final var filePath = "unknown.txt";
+
+    assertThatThrownBy(() -> {
+       new LoadEmployeesCelebratingBirthdayOn_New(filePath, date("2024/10/08")).execute();
+    }).isInstanceOf(FileNotFoundException.class)
+        .hasMessageContaining(filePath);
+  }
+
+  protected String line(final String text) {
+    return text;
+  }
+
+  protected String header() {
+    return "last_name, first_name, date_of_birth, email";
+  }
+
+  protected LocalDate date(String value) {
+    return LocalDate.parse(value, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+  }
+
+  @Test
+  void oneEmployeeCelebrateBirthday() throws Exception {
+    final var filePath = employeeFile(
+        header(),
+        line("Doe, John, 1982/10/08, john.doe@foobar.com"),
+        line("Ann, Mary, 1975/03/11, mary.ann@foobar.com")
+    );
+    final var employees = new LoadEmployeesCelebratingBirthdayOn_New(filePath, date("2024/10/08")).execute();
+
+    assertThat(employees.size()).isEqualTo(1);
+    assertThat(employees.get(0).firstName()).isEqualTo("John");
+    assertThat(employees.get(0).email()).isEqualTo("john.doe@foobar.com");
+  }
+
+  @Test
+  void manyEmployeesCelebrateBirthday() throws Exception {
+    final var filePath = employeeFile(
+        header(),
+        line("Doe, John, 1982/10/08, john.doe@foobar.com"),
+        line("Ann, Mary, 1975/03/11, mary.ann@foobar.com"),
+        line("Rossi, Mario, 2000/10/08, mario.rossi@foobar.com")
+    );
+    final var employees = new LoadEmployeesCelebratingBirthdayOn_New(filePath, date("2024/10/08")).execute();
+
+    assertThat(employees.size()).isEqualTo(2);
+    assertThat(employees.get(0).firstName()).isEqualTo("John");
+    assertThat(employees.get(1).firstName()).isEqualTo("Mario");
+  }
+
+  @Test
+  void combinationTests() throws Exception {
+    final var filePath =
+        employeeFile(
+            header(),
+            line("Doe, John, 1982/10/08, john.doe@foobar.com"),
+            line("Ann, Mary, 1975/03/11, mary.ann@foobar.com"),
+            line("Rossi, Mario, 2000/10/08, mario.rossi@foobar.com"));
+
+    CombinationApprovals.verifyAllCombinations(
+        (file, today) -> {
+          try {
+            return new LoadEmployeesCelebratingBirthdayOn_New(file, date(today)).execute();
+          } catch (IOException e) {
+            throw new RuntimeException(e);
+          }
+        },
+        new String[] {filePath},
+        new String[] {"2024/10/08", "2024/10/09", "2024/03/11"});
   }
 }
--- src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_OldTest.java
+++
@@ -1,9 +0,0 @@
-package io.doubleloop;
-
-public class LoadEmployeesCelebratingBirthdayOn_OldTest extends LoadEmployeesCelebratingBirthdayOnContractTest {
-  @Override
-  protected LoadEmployeesCelebratingBirthdayOnContract createLoadEmployeesCelebratingBirthdayOn(String filePath, String today) {
-    return new LoadEmployeesCelebratingBirthdayOn_Old(filePath, date(today));
-  }
-
-}
