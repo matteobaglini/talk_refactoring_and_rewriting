f7ca94df65f7b18a8f765ca5529bf1fd8cce2c92
Matteo Baglini <matteo.baglini@gmail.com>
2024-04-17 12:18:49 +0200

    refactor - branch by abstraction: add more more notes

--- src/main/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOnContract.java
+++ src/main/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOnContract.java
@@ -3,6 +3,9 @@ package io.doubleloop;
 import java.io.IOException;
 import java.util.List;
 
+// REFACTORING: branch by abstraction (https://martinfowler.com/bliki/BranchByAbstraction.html)
+
+// REFACTOR STEP: create empty "new" implementation
 public interface LoadEmployeesCelebratingBirthdayOnContract {
   List<Employee> execute() throws IOException;
 }
