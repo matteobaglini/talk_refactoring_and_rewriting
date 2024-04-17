a77e08666522b9daa6ff28c168fd38c7c45e8e07
Matteo Baglini <matteo.baglini@gmail.com>
2024-04-17 11:59:44 +0200

    rewrite: remove different implementations

--- src/main/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn.java
+++ src/main/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn.java
@@ -1,20 +1,11 @@
 package io.doubleloop;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 public class LoadEmployeesCelebratingBirthdayOn {
   private final String fileName;
@@ -39,51 +30,5 @@ public class LoadEmployeesCelebratingBirthdayOn {
       }
     }
     return result;
-  }
-
-  public List<Employee> execute_rewritten_with_external_lib() throws IOException {
-    final var csvFormat = CSVFormat.DEFAULT.builder()
-        .setHeader(Headers.class)
-        .setSkipHeaderRecord(true)
-        .setTrim(true)
-        .setIgnoreEmptyLines(true)
-        .setIgnoreSurroundingSpaces(true)
-        .build();
-    final var reader = Files.newBufferedReader(Path.of(fileName));
-
-    return CsvRecords.from(reader)
-        .parse(csvFormat)
-        .map(x ->
-            new Employee(
-                x.get(Headers.last_name),
-                x.get(Headers.first_name),
-                LocalDate.parse(x.get(Headers.date_of_birth), DateTimeFormatter.ofPattern("yyyy/MM/dd")),
-                x.get(Headers.email)))
-        .filter(e -> e.isBirthday(today))
-        .toList();
-  }
-
-  private enum Headers {
-    last_name,
-    first_name,
-    date_of_birth,
-    email
-  }
-
-  private static class CsvRecords {
-    private final Reader reader;
-
-    private CsvRecords(Reader reader) {
-      this.reader = reader;
-    }
-
-    public static CsvRecords from(Reader reader) {
-      return new CsvRecords(reader);
-    }
-
-    public Stream<CSVRecord> parse(CSVFormat format) throws IOException {
-      final var records = format.parse(reader);
-      return StreamSupport.stream(records.spliterator(), false);
-    }
   }
 }
