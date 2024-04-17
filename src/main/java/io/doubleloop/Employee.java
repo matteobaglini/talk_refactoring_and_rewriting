347f2461ec6aadcb0c5b977fb327bc9b65a0f488
Matteo Baglini <matteo.baglini@gmail.com>
2024-04-17 11:47:03 +0200

    refactor - move function: celan up

--- src/main/java/io/doubleloop/Employee.java
+++ src/main/java/io/doubleloop/Employee.java
@@ -5,13 +5,12 @@ import java.time.format.DateTimeFormatter;
 
 public record Employee(String firstName, String lastName, LocalDate birthDate, String email) {
   static Employee parseLine(String str) {
-    String[] employeeData = str.split(", ");
-    Employee employee = new Employee(
+    final var employeeData = str.split(", ");
+    return new Employee(
         employeeData[1],
         employeeData[0],
         LocalDate.parse(employeeData[2], DateTimeFormatter.ofPattern("yyyy/MM/dd")),
         employeeData[3]);
-    return employee;
   }
 
   public boolean isBirthday(LocalDate now) {
