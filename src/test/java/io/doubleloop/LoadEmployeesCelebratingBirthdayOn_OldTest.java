b2acef1ae603f2d77f0e06461f8eef671da905da
Matteo Baglini <matteo.baglini@gmail.com>
2024-04-17 12:34:21 +0200

    test - contract testing: pull members up

--- src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_OldTest.java
+++ src/test/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_OldTest.java
@@ -2,29 +2,11 @@ package io.doubleloop;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 // REFACTORING: introduce contract tests
 
-// REFACTORING STEP: pull members up (utility functions)
 public class LoadEmployeesCelebratingBirthdayOn_OldTest extends LoadEmployeesCelebratingBirthdayOnContractTest {
-  private Path testDataPath;
-
-  @BeforeEach
-  void setUp() {
-    testDataPath = Paths.get("test-data-bs");
-    final var testDataDir = testDataPath.toFile();
-    testDataDir.deleteOnExit();
-    testDataDir.mkdirs();
-  }
 
   @Test
   void noEmployeeCelebrateBirthday() throws Exception {
@@ -37,22 +19,4 @@ public class LoadEmployeesCelebratingBirthdayOn_OldTest extends LoadEmployeesCel
     assertThat(employees.size()).isEqualTo(0);
   }
 
-  private String employeeFile(String... lines) throws IOException {
-    final var filePath = Paths.get(testDataPath.toString(), "employee_data.txt");
-    filePath.toFile().deleteOnExit();
-    Files.write(filePath, Arrays.asList(lines));
-    return filePath.toString();
-  }
-
-  private String line(final String text) {
-    return text;
-  }
-
-  private String header() {
-    return "last_name, first_name, date_of_birth, email";
-  }
-
-  private LocalDate date(String value) {
-    return LocalDate.parse(value, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-  }
 }
