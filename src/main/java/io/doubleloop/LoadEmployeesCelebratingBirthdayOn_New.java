4bc03053446a48727a03f5e11deca5627a34afdd
Matteo Baglini <matteo.baglini@gmail.com>
2024-04-17 12:49:31 +0200

    test - contract testing: green bar on missing file

--- src/main/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_New.java
+++ src/main/java/io/doubleloop/LoadEmployeesCelebratingBirthdayOn_New.java
@@ -1,10 +1,11 @@
 package io.doubleloop;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 
 public class LoadEmployeesCelebratingBirthdayOn_New implements LoadEmployeesCelebratingBirthdayOnContract {
@@ -20,10 +21,14 @@ public class LoadEmployeesCelebratingBirthdayOn_New implements LoadEmployeesCele
   @Override
   public List<Employee> execute() throws IOException {
     final int HEADER = 1;
-    return Files.readAllLines(Path.of(fileName)).stream()
-        .skip(HEADER)
-        .map(Employee::parseLine)
-        .filter(e -> e.isBirthday(today))
-        .toList();
+    try {
+      return Files.readAllLines(Path.of(fileName)).stream()
+          .skip(HEADER)
+          .map(Employee::parseLine)
+          .filter(e -> e.isBirthday(today))
+          .toList();
+    } catch (NoSuchFileException e) {
+      throw new FileNotFoundException("Invalid file path: " + fileName);
+    }
   }
 }
